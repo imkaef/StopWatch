2f38c16f7e0ddae224ec51bc061691387f9ec543
Sergey <Sergey.popov.fly@gmail.com>
2019-10-07 20:43:00 +0300

    Reload on change orientation

---
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.kt
@@ -0,0 +1,50 @@
+package com.example.stopwatch
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_stopwatch.*
+import java.util.*
+
+
+class StopwatchActivity : AppCompatActivity() {
+
+
+    private var seconds: Int = 0
+    private var running: Boolean = false
+
+    final val handler = Handler()
+    private fun runTimer (){
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = seconds % 3600 / 60
+                val secs = seconds % 60
+                val time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs)
+                time_view.setText(time)
+                if (running) {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stopwatch)
+        runTimer()
+
+        start_button.setOnClickListener {
+            running=true
+        }
+        stop_button.setOnClickListener {
+            running=false
+        }
+        reset_button.setOnClickListener {
+
+            running=false
+            seconds = 0
+        }
+    }
+}
